146c0159e6e1af697d30f9f9e1622c6adce1bc2f
karina A <karina.arevalo@hotmail.com>
2022-12-06 19:06:38 -0600

    @

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -12,5 +12,5 @@
   <h1 class="py-4 text-center text-lg font-thin text-red-500">
     If you can not attend the appointment please call us 24 hours to change your appointment or cancel thank you !
   </h1>
-  <h1 class="py-4 text-center text-xl font-thin text-white">Ph:800-811-7688</h1>
+  <h1 class="py-4 text-center text-2xl font-thin text-red-500">Ph:800-811-7688</h1>
 </template>
