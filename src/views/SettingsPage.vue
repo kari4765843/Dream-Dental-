b2e5fee88b488b2f106cb6bbd9276dbfe399ed33
karina A <karina.arevalo@hotmail.com>
2022-12-06 18:59:41 -0600

    Adding information

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -8,4 +8,9 @@
     Remember to arrive 10 min early for your first time paper work and if you have insurance please make sure to bring
     it with you! Thank you , Ms. Leslie we see you here ! Dream Dental
   </h1>
+
+  <h1 class="py-4 text-center text-lg font-thin text-red-500">
+    If you can not attend the appointment please call us 24 hours to change your appointment or cancel thank you !
+  </h1>
+  <h1 class="py-4 text-center text-xl font-thin text-white">Ph:800-811-7688</h1>
 </template>
