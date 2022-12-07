9fc8a171aa1edc286b666db3163cf675309cc750
karina A <karina.arevalo@hotmail.com>
2022-12-06 18:54:37 -0600

    #

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -1,10 +1,10 @@
 <template>
-  <h1 class="text-center text-lg font-thin text-white">
+  <h1 class="py-4 text-center text-lg font-thin text-black">
     Date: 12-22-2022 | Time:8:30AM | Where: Dream Dental office, 7676 S Red Rd #4, Fox, TX 78577, United States|Dental
     visit🦷
   </h1>
 
-  <h1 class="text-center text-lg font-thin text-red-500">
+  <h1 class="py-4 text-center text-lg font-thin text-red-500">
     Remember to arrive 10 min early for your first time paper work and if you have insurance please make sure to bring
     it with you! Thank you , Ms. Leslie we see you here ! Dream Dental
   </h1>
